378d03c9de7fdb8dc5067862b40bd48401047321
蘇龍 <sulong@soryuunoMacBook-Pro.local>
2020-02-24 17:51:48 +0900

    kaminari page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,9 @@
 class EventsController < ApplicationController
-
+    before_action :set_event, :only=>[ :show, :edit, :update, :destroy]
     #get /events/index
     #get /events
     def index
-        @events=Event.all
+        @events=Event.page(params[:page]).per(10)
     end
     def new
         @event=Event.new
@@ -11,31 +11,50 @@ class EventsController < ApplicationController
     
     def create
         @event=Event.new(event_params)
-        @event.save
+
+       if @event.save
+        flash[:notice]="新增成功"
         redirect_to :action=>:index
+       else
+        render :action=>:new
+       end
     end
+          
+        
     
     def show
-        @event=Event.find(params[:id]) 
+        
+        @page_title=@event.name
     end
     
     def edit
-        @event=Event.find(params[:id]) 
+       
     end
     
     def update
-        @event=Event.find(params[:id])
-        @event.update(event_params)
-        redirect_to :action=>:index
+        
+         if@event.update(event_params)
+            flash[:notice]="新增成功" 
+            redirect_to :action=>:show,:id=>@event
+         else
+            #redirect_to :action=>:edit,:id => @event
+            render :action=>:edit
+         end
     end
     
 
     def destroy
-        @event=Event.find(params[:id])
+        
         @event.destroy
+        flash[:alert]="新增成功"
         redirect_to :action=>:index 
     end
     
+    private
+
+    def set_event
+        @event=Event.find(params[:id])
+    end
 
     def event_params
         params.require(:event).permit(:name,:description)
